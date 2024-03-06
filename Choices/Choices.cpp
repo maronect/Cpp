264871214d86651d7a7bed92d76ca0f6f94f5674
Marone <maronect@gmail.com>
2024-03-06 18:15:17 -0300

    Making the actions clearer

--- Choices/Choices.cpp
+++ Choices/Choices.cpp
@@ -15,7 +15,7 @@ int main(){
     while (keep == true)
     {
         cout << "\nChoose an option to operate:\nP - Print numbers\nA - Add a number\nM - Display mean of numbers"
-        "\nS - Display smallest number\nL - Display the largest number\nQ - Quit" << endl;
+        "\nS - Display smallest number\nL - Display the largest number\nC - Clear list\nQ - Quit" << endl;
         cin >> choice;
 
         if(choice == 'p' || choice == 'P'){
@@ -28,11 +28,11 @@ int main(){
             int num {};
             cin >> num;
             vet.push_back(num);
-            cout << num <<" added";
+            cout << num <<" Added";
         }else if(choice == 'm'||choice == 'M'){
             int tot{0};
             if(vet.empty()){
-                cout << "unable to calculate the mean - no data";
+                cout << "Unable to calculate the mean - no data";
                 continue;
             }
             for (auto i : vet) {
@@ -40,6 +40,10 @@ int main(){
             }
             cout << "Mean: " << tot / vet.size();
         }else if(choice == 's'||choice == 'S'){
+            if(vet.empty()){
+                cout << "Unable to get the smallest number - no data";
+                continue;
+            }
             min = vet.front();
             for(auto i:vet){
                 if(min > i) min = i;
@@ -47,12 +51,19 @@ int main(){
             cout << "The smallest number is " << min;
 
         }else if(choice == 'l'||choice == 'L'){
+            if(vet.empty()){
+                cout << "Unable to get the largest number - no data";
+                continue;
+            }
             max = vet.front();
             for(auto i:vet){
                 if(max < i) max = i;
             }
             cout << "The largest number is " << max;
 
+        }else if(choice == 'c'||choice == 'C'){
+            vet.clear();
+            cout << "The list now is empty";
         }else if(choice == 'q'||choice == 'Q'){
             cout << "Goodbye"; 
             keep = false;
